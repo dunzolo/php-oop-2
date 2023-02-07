5fd9da54c9f011f3d5b1d8da127cf5b269cf5416
dunzolo <rossidavide108@gmail.com>
2023-02-07 17:05:55 +0100

    added class Animal to Products

---
+++ Models/Animal.php
@@ -0,0 +1,11 @@
+<?php
+    class Animal{
+        public $name;
+        public $icon;
+        
+        function __construct($_name, $_icon){
+            $this->name=$_name;
+            $this->icon=$_icon;
+        }
+    }
+?>
